6c40d04c2b8f21faa8aa8e09b58e3a3e5d8d6ae4
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-09-26 14:48:25 -0700

    select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,40 +1,31 @@
-#include <stdio.h>
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in ascending order using the
- *                  Selection sort algorithm.
- * @array: The array to be sorted.
- * @size: The number of elements in the array.
+ * selection_sort - sort list with selection_sort
+ * @array: The array to be printed
+ * @size: Number of elements in @array
  */
 void selection_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
+	size_t i, c, less, tmp, swap;
+
+	if (array == NULL)
 		return;
-	
-	size_t i; 
-	size_t j;
-	size_t min_idx;
-	int temp;
-
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		min_idx = i;
-
-		for (j = i + 1; j < size; j++)
+		for (less = i, c = i; c < size; c++)
+			if (array[c] < array[less])
+			{
+				less = c;
+				swap = 1;
+			}
+		if (swap == 1)
 		{
-			/* Find the index of the minimum element in the unsorted portion of the array*/
-			if (array[j] < array[min_idx])
-				min_idx = j;
-		}
-		/* Swap the found minimum element with the current element (if not the same) */
-		if (min_idx != i)
-		{
-			temp = array[i];
-			array[i] = array[min_idx];
-			array[min_idx] = temp;
-			print_array(array, size); /* Print the array after each swap */
+			tmp = array[less];
+			array[less] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+			swap = 0;
 		}
 	}
 }
-
